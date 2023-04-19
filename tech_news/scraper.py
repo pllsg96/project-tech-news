e9a61b727637e19990dd47ff28f96e6483bfe7e9
Pedro Luiz <pedroluizlsg@gmail.com>
2023-04-19 15:53:22 -0300

    feat: implementa scrape_news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from pprint import pprint
 
 
 # Requisito 1
@@ -23,7 +24,7 @@ def fetch(url):
 def scrape_updates(html_content):
     selector = Selector(html_content)
     links_to_news = []
-    all_news = selector.css('article.entry-preview')
+    all_news = selector.css("article.entry-preview")
     for new in all_news:
         link = new.css("a::attr(href)").get()
         links_to_news.append(link)
@@ -33,13 +34,36 @@ def scrape_updates(html_content):
 # Requisito 3
 def scrape_next_page_link(html_content):
     selector = Selector(html_content)
-    link_to_next_page = selector.css('a.next.page-numbers::attr(href)').get()
+    link_to_next_page = selector.css("a.next.page-numbers::attr(href)").get()
     return link_to_next_page
 
 
 # Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    # news_data = []
+    news_url = selector.css("link[rel='canonical']::attr(href)").get()
+    news_title = selector.css("h1.entry-title::text").get()
+    news_title = news_title.strip()
+    news_timestamp = selector.css("li.meta-date::text").get()
+    news_writer = selector.css("a.url.fn.n::text").get()
+    news_reading_time = selector.css("li.meta-reading-time::text").get()
+    news_reading_time = news_reading_time.split(" ")
+    news_summary = selector.css(
+        "div.entry-content:first-of-type > p:nth-of-type(1) *::text"
+        ).getall()
+    news_summary = ("".join(news_summary)).strip()
+    news_category = selector.css("span.label::text").get()
+
+    return {
+        "url": news_url,
+        "title": news_title,
+        "timestamp": news_timestamp,
+        "writer": news_writer,
+        "reading_time": int(news_reading_time[0]),
+        "summary": news_summary,
+        "category": str(news_category),
+    }
 
 
 # Requisito 5
@@ -48,6 +72,7 @@ def get_tech_news(amount):
 
 
 if __name__ == "__main__":
-    site_trybe = ('https://blog.betrybe.com')
-    xomps = scrape_updates(site_trybe)
-    print(xomps)
+    url = ("https://blog.betrybe.com/carreira/frases-de-lideranca/")
+    site_trybe = fetch(url)
+    xomps = scrape_news(site_trybe)
+    pprint(xomps)
